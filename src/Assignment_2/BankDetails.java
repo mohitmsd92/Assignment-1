075bb569f0e9fbb3ae53e505b351d859e5a0fddf
Mohit <mohit.dhande@tothenew.com>
2022-09-29 12:09:20 +0530

    Assignment 2 Updated

---
+++ src/Assignment_2/BankDetails.java
@@ -0,0 +1,23 @@
+package Assignment_2;
+
+public class BankDetails {
+	public void SBI() {
+		System.out.println("Welcome To SBI Noida");
+		System.out.println("Branch - Sector 18 Noida");
+		System.out.println("IFSC - SBIN000012");
+		System.out.println("ROI - 5% on Saving account");
+	}
+	public void ICICI() {
+		System.out.println("Welcome To ICICI Noida");
+		System.out.println("Branch - Sector 144 Noida");
+		System.out.println("IFSC - ICICI00099");
+		System.out.println("ROI - 4% on Saving account");
+	}
+	public void BOI() {
+		System.out.println("Welcome To BOI Noida");
+		System.out.println("Branch - Sector 62 Noida");
+		System.out.println("IFSC - BKID000072");
+		System.out.println("ROI - 4.5% on Saving account");
+	}
+
+}
