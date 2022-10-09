350a7cbc7537be98e45a43f6ffc2535c7227086a
mohitmsd92 <114056321+mohitmsd92@users.noreply.github.com>
2022-10-09 10:54:11 +0530

    Add files via upload

---
+++ src/Assignment_5/Q11.java
@@ -0,0 +1,26 @@
+package Assignment_5;
+
+record Student(String name,int id, int age){
+    static int counter = 0;
+    public Student(String name, int id, int age) {
+        this.name = name;
+        this.id = id;
+        this.age = age;
+        counter++;
+    }
+
+}
+public class Q11 {
+    public static void main(String[] args) {
+        Student student1 = new Student("Mohit",101,22);
+        Student student2 = new Student("Dhaval",102,21);
+        System.out.println(student1);
+        System.out.println(student1.age());
+        System.out.println(student1.id());
+        System.out.println(student1.name());
+        System.out.println(student1.hashCode());
+        System.out.println(student2.hashCode());
+        System.out.println(student1.equals(student2));
+        System.out.println(Student.counter);
+    }
+}
